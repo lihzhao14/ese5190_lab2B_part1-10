6fb0be02a2ce419fc3d9ea12ff5cbf6a1af8ca7f
lihzhao14 <lihzhao@seas.upenn.edu>
2022-11-16 14:41:01 -0500

    add part_4

---
+++ 04_slow_motion/part4_slow_motion.py
@@ -0,0 +1,57 @@
+import serial
+import time
+
+
+##define the board
+port='COM6'
+Myboard=serial.Serial(port)
+Myboard.baudrate=115200
+print(Myboard)
+
+while 1:
+    print('Press 1 for record; Press 2 for replay')
+    fun_sel=input()
+    print(fun_sel)
+    print('please select the frequency you want to record or replay')
+    freq=input()
+##Record mode
+    if fun_sel=='1':
+        count=0
+        while count<=100:
+            board_output=Myboard.readline()
+            print(board_output)
+            board_output=str(board_output)[2:-5]+'\n'
+            board_output=board_output.split(',')
+            time.sleep(1/float(freq))
+            count=count+1
+            print(count)
+            logs=open('logs.txt',mode='a')
+            logs.writelines(board_output)
+            logs.close
+            if count>100:
+                print('Record finished')
+                Myboard.close
+
+
+##Replay mode
+    elif fun_sel=='2':
+        count=0
+        while count<=100:
+            logs=open('logs.txt',mode='r+')
+            command=logs.readlines()
+            ##print(command[count])
+            if command[count]=='70e0300\n':
+                print('70e0300')
+                Myboard.write(bytearray('A','ascii'))
+            elif command[count]=='300\n':
+                print('300')
+                Myboard.write(bytearray('B','ascii'))
+            count=count+1
+            time.sleep(1/float(freq))
+            if count>100:
+                print('Replay finished')
+                logs.close
+                Myboard.close
+    
+    else:
+        break
